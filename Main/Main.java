fdd5c94c19c32cff68045c7f44e18e278a1317bc
buch95 <sergiibuchel@yahoo.com>
2020-01-28 17:15:14 +0200

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -5,40 +5,65 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int hWNum;
-		System.out.println("Please input 1 2 or 3 to select homework task");
+		System.out.println("Please input your number select homework task");
 		hWNum = sc.nextInt();
-		if(hWNum == 1) {
-			System.out.println("Please input your 5 digit number:");
-			int numr = sc.nextInt();
-			String number = String.valueOf(numr);
-			for (int i = 0; i< number.length(); i++) {
-				int outputNum = Character.digit(number.charAt(i), 10);
-				// radix is number alphabet radix 2 is binary, radix 10 is 0 1 2 3 ... 9  
-				// Character.digit(what data to convert, radix)
-				System.out.println(outputNum);
+		if (hWNum == 1) {
+			System.out.println("Input pattern repeat time");
+			int stripe = sc.nextInt();
+			System.out.println("Input num of rows");
+			int height = sc.nextInt();
+			for (int j = 1; j <= height; j++) {
+				for (int i = 1; i <= stripe; i++) {
+					if (i % 2 == 0) {
+						System.out.print("***");
+					} else {
+						System.out.print("+++");
+					}
+				}
+				System.out.println();
 			}
-		}else if(hWNum == 2) {
-			System.out.println("Please input side a length");
-			int a = sc.nextInt();
-			System.out.println("Please input side b length");
-			int b = sc.nextInt();
-			System.out.println("Please input side c length");
-			int c = sc.nextInt();
-			
-			double p = (a+b+c)/2;	
-			double s  = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-			System.out.println("Square area of your triangle " + s);
-			
-		}else {
-			System.out.println("Please input radius");
-			int r = sc.nextInt();
-			double c = 2*Math.PI*r;
-			
-			System.out.println("Circumference is " + String.format("%.2f", c) );
-		};	
+
+		} else if (hWNum == 2) {
+			System.out.println("Input your number");
+			int facInp = sc.nextInt();
+			int facOut = 1;
+			if (4 < facInp && facInp < 16) {
+				for (int i = 1; i <= facInp; i++) {
+					facOut = facOut * i;
+				}
+				System.out.println(facOut);
+			}
+		} else if (hWNum == 3) {
+			for (int i = 0; i < 10; i++) {
+				int sum = i * 5;
+				System.out.println(i + " x 5 = " + sum);
+			}
+		} else {
+			System.out.println("Input width");
+			int width = sc.nextInt();
+			System.out.println("Input height");
+			int height = sc.nextInt();
+			String str = "*";
+			for (int i = 1; i <= height; i++) {
+				for (int j = 1; j < width; j++) {
+					if (i == 1 || i == height) {
+						System.out.print(str);
+					} else {
+						if (j == 1 || j == width-1) {
+							System.out.print(str);
+						} else {
+							System.out.print(" ");
+						}
+					}
+				}
+				System.out.println();
+			}
+
+		}
+		;
 	}
 
-}	
+}
